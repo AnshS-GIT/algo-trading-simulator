dd567bb8f2ccb01babff023ba7ab3c6b57ae51c2
Ansh Sharma <sansh2405@gmail.com>
2025-12-15 18:43:36 +0530

    fixed backend url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5001/api',
+    baseURL: 'https://algo-trading-simulator.onrender.com/api',
 });
 
 // Add token to headers if it exists
